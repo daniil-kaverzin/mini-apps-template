dd6e91e866d2a88f358b3b45100bd73e17304ee7
Daniil Kaverzin <daniil.kaverzin@outlook.com>
2021-03-14 12:04:05 +0700

    🕯 Added core-js (Fix for old androids)

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+import 'core-js/features/map';
+import 'core-js/features/set';
+import 'core-js/features/promise';
+import 'core-js/features/symbol';
+import 'core-js/features/object';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
